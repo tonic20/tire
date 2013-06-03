6939c80be133b7150bead27b4f5b5c305dd904cd
Alex Pylko <alexpylko@gmail.com>
2013-06-03 13:52:46 +0200

    Remove class_name from options

--- lib/tire/results/collection.rb
+++ lib/tire/results/collection.rb
@@ -129,7 +129,7 @@ module Tire
                                "document has no _type property." unless type
 
           begin
-            klass = @options[:class_name].nil? ? type.camelize.constantize : @options[:class_name].constantize
+            klass = @options[:class_name].nil? ? type.camelize.constantize : @options.delete(:class_name).constantize
           rescue NameError => e
             raise NameError, "You have tried to eager load the model instances, but " +
                              "Tire cannot find the model class '#{type.camelize}' " +
